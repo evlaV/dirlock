3473ef9c65d0c130b3499ac7bd70aac20c235538
Alberto Garcia <berto@igalia.com>
2025-05-08 13:57:38 +0200

    Add test for Pbkdf2

--- dirlock/src/kdf.rs
+++ dirlock/src/kdf.rs
@@ -56,3 +56,53 @@ impl Pbkdf2 {
         pbkdf2_hmac::<sha2::Sha512>(pass, salt, self.iterations, result);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use anyhow::Result;
+    use base64::prelude::*;
+    use super::Pbkdf2;
+
+    const PBKDF2_DATA: &[[&str; 5]] = &[
+        [
+            "1234", // password
+            "vdntwBKZ5ahXJly/1wNhyZzYKS13byCW22Lt/YQgMQE=", // salt
+            "65535", // iterations
+            "32", // output length
+            "RMHUPAZRCII9HBuA3LvxcUWrdChXqS5J46tOBmNJQkM=", // expected result
+        ],
+        [
+            "3rogOH9HcseUlJP0n2NcYpA7KIcxEtPq", // password
+            "zDF2am9A9I3TxPCRuNeTwnbmMtfK9Tgq53Gl8e0fDek=", // salt
+            "1000000", // iterations
+            "64", // output length
+            "ZMmGN7V7qj+qtzgZYCtUnaTnX3ICAqAP6rIJvWaEceNVilaWKQ3PofHtie8tRrYOtwVpWxaIbD2SZkyL9QXwlQ==", // expected result
+        ],
+        [
+            "aPQ8jMpc", // password
+            "Ig35AsUoPr8=", // salt
+            "50", // iterations
+            "16", // output length
+            "ZdPyXwhKywPAihNOjJtQqg==", // expected result
+        ],
+    ];
+
+    #[test]
+    fn test_pbkdf2() -> Result<()> {
+        for item in PBKDF2_DATA {
+            let pass = item[0].as_bytes();
+            let salt = BASE64_STANDARD.decode(item[1]).unwrap();
+            let iter = str::parse(item[2]).unwrap();
+            let len = str::parse(item[3]).unwrap();
+            let expected = BASE64_STANDARD.decode(item[4]).unwrap();
+
+            let mut result = vec![0u8; len];
+
+            let kdf = Pbkdf2::new(iter);
+            kdf.derive(pass, &salt, &mut result);
+            assert_eq!(result, expected, "PBKDF2 output doesn't match the expected value");
+        }
+
+        Ok(())
+    }
+}
